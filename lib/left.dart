3718cbbe2d479319c0951e9f4028a8d327d9a76b
yizhen47 <1503242062@qq.com>
2024-12-10 13:42:20 +0800

    q

--- lib/left.dart
+++ lib/left.dart
@@ -7,6 +7,10 @@ import 'package:flutter/material.dart';
 class MyApp2 extends StatelessWidget {
   const MyApp2({super.key});
 
+
+
+
+
   //这修改页面2的内容
   @override
   Widget build(BuildContext context) {
