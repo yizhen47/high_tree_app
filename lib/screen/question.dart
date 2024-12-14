78a5229dad65dbe8366973e77972062086a9cc4d
yizhen47 <1503242062@qq.com>
2024-12-14 22:06:27 +0800

    悬浮按钮

--- lib/screen/question.dart
+++ lib/screen/question.dart
@@ -15,167 +15,190 @@ class QuestionScreen extends StatefulWidget {
 Card buildCard(
     final String knowledgepoint, final String question, final String? answer) {
   return Card(
-    color: Colors.white,
-    elevation: 4,
-    child: SingleChildScrollView(
-        child: Padding(
-      padding: const EdgeInsets.all(15),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(
-            width: double.infinity,
-          ),
-          Card(
-            color: Colors.blueAccent,
-            margin: const EdgeInsets.fromLTRB(0, 4, 4, 4),
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      color: Colors.white,
+      elevation: 4,
+      child: SizedBox(
+        height: double.infinity,
+        child: SingleChildScrollView(
             child: Padding(
-              padding: const EdgeInsets.fromLTRB(6, 2, 6, 2),
-              child: Text(
-                knowledgepoint,
-                style: const TextStyle(color: Colors.white, fontSize: 14),
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                width: double.infinity,
               ),
-            ),
-          ),
-          Text(
-            question,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 94, 94, 94),
-            ),
-          ),
-          // const Column(
-          //   mainAxisAlignment: MainAxisAlignment.start,
-          //   crossAxisAlignment: CrossAxisAlignment.start,
-          //   verticalDirection: VerticalDirection.down,
-          //   children: [
-          //     Text(
-          //       '''可以参考以下选项''',
-          //       style: TextStyle(
-          //         fontSize: 14,
-          //         // fontStyle: FontStyle.italic,
-          //         color: Colors.grey,
-          //         fontFamily: 'Times New Roman',
-          //       ),
-          //     ),
-          //     Row(
-          //       children: [
-          //         Text(
-          //           'A ',
-          //           style: TextStyle(
-          //             fontSize: 14,
-          //             fontWeight: FontWeight.bold,
-          //             color: Colors.black,
-          //             fontFamily: 'Times New Roman',
-          //           ),
-          //         ),
-          //         SizedBox(
-          //           width: 10,
-          //         ),
-          //         Text(
-          //           "不知道",
-          //           style: TextStyle(
-          //             fontSize: 14,
-          //             fontWeight: FontWeight.bold,
-          //             color: Colors.grey,
-          //             fontFamily: 'Times New Roman',
-          //           ),
-          //         ),
-          //       ],
-          //     ),
-          //     Row(
-          //       children: [
-          //         Text(
-          //           'B ',
-          //           style: TextStyle(
-          //             fontSize: 14,
-          //             fontWeight: FontWeight.bold,
-          //             color: Colors.black,
-          //             fontFamily: 'Times New Roman',
-          //           ),
-          //         ),
-          //         SizedBox(
-          //           width: 10,
-          //         ),
-          //         Text(
-          //           "TD,以后不再显示",
-          //           style: TextStyle(
-          //             fontSize: 14,
-          //             fontWeight: FontWeight.bold,
-          //             color: Colors.grey,
-          //             fontFamily: 'Times New Roman',
-          //           ),
-          //         ),
-          //       ],
-          //     ),
-          //     Row(
-          //       children: [
-          //         Text(
-          //           'C ',
-          //           style: TextStyle(
-          //             fontSize: 14,
-          //             fontWeight: FontWeight.bold,
-          //             color: Colors.black,
-          //             fontFamily: 'Times New Roman',
-          //           ),
-          //         ),
-          //         SizedBox(
-          //           width: 10,
-          //         ),
-          //         Text(
-          //           "钝角",
-          //           style: TextStyle(
-          //             fontSize: 14,
-          //             fontWeight: FontWeight.bold,
-          //             color: Colors.grey,
-          //             fontFamily: 'Times New Roman',
-          //           ),
-          //         ),
-          //       ],
-          //     ),
-          //   ],
-          // ),
-          SizedBox(
-            height: 30,
-            child: Container(
-              alignment: Alignment.center,
-              child: const TDDivider(
-                color: Colors.black38,
+              Card(
+                color: Colors.blueAccent,
+                margin: const EdgeInsets.fromLTRB(0, 4, 4, 4),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(6, 2, 6, 2),
+                  child: Text(
+                    knowledgepoint,
+                    style: const TextStyle(color: Colors.white, fontSize: 14),
+                  ),
+                ),
               ),
-            ),
+              Text(
+                question,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 94, 94, 94),
+                ),
+              ),
+              // const Column(
+              //   mainAxisAlignment: MainAxisAlignment.start,
+              //   crossAxisAlignment: CrossAxisAlignment.start,
+              //   verticalDirection: VerticalDirection.down,
+              //   children: [
+              //     Text(
+              //       '''可以参考以下选项''',
+              //       style: TextStyle(
+              //         fontSize: 14,
+              //         // fontStyle: FontStyle.italic,
+              //         color: Colors.grey,
+              //         fontFamily: 'Times New Roman',
+              //       ),
+              //     ),
+              //     Row(
+              //       children: [
+              //         Text(
+              //           'A ',
+              //           style: TextStyle(
+              //             fontSize: 14,
+              //             fontWeight: FontWeight.bold,
+              //             color: Colors.black,
+              //             fontFamily: 'Times New Roman',
+              //           ),
+              //         ),
+              //         SizedBox(
+              //           width: 10,
+              //         ),
+              //         Text(
+              //           "不知道",
+              //           style: TextStyle(
+              //             fontSize: 14,
+              //             fontWeight: FontWeight.bold,
+              //             color: Colors.grey,
+              //             fontFamily: 'Times New Roman',
+              //           ),
+              //         ),
+              //       ],
+              //     ),
+              //     Row(
+              //       children: [
+              //         Text(
+              //           'B ',
+              //           style: TextStyle(
+              //             fontSize: 14,
+              //             fontWeight: FontWeight.bold,
+              //             color: Colors.black,
+              //             fontFamily: 'Times New Roman',
+              //           ),
+              //         ),
+              //         SizedBox(
+              //           width: 10,
+              //         ),
+              //         Text(
+              //           "TD,以后不再显示",
+              //           style: TextStyle(
+              //             fontSize: 14,
+              //             fontWeight: FontWeight.bold,
+              //             color: Colors.grey,
+              //             fontFamily: 'Times New Roman',
+              //           ),
+              //         ),
+              //       ],
+              //     ),
+              //     Row(
+              //       children: [
+              //         Text(
+              //           'C ',
+              //           style: TextStyle(
+              //             fontSize: 14,
+              //             fontWeight: FontWeight.bold,
+              //             color: Colors.black,
+              //             fontFamily: 'Times New Roman',
+              //           ),
+              //         ),
+              //         SizedBox(
+              //           width: 10,
+              //         ),
+              //         Text(
+              //           "钝角",
+              //           style: TextStyle(
+              //             fontSize: 14,
+              //             fontWeight: FontWeight.bold,
+              //             color: Colors.grey,
+              //             fontFamily: 'Times New Roman',
+              //           ),
+              //         ),
+              //       ],
+              //     ),
+              //   ],
+              // ),
+              SizedBox(
+                height: 30,
+                child: Container(
+                  alignment: Alignment.center,
+                  child: const TDDivider(
+                    color: Colors.black38,
+                  ),
+                ),
+              ),
+              const Text(
+                "解析",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontFamily: 'Times New Roman',
+                ),
+              ),
+              Text(
+                answer ?? "暂无解析",
+                style: const TextStyle(
+                  fontSize: 14,
+                  fontFamily: 'Times New Roman',
+                ),
+              ),
+            ],
           ),
-          const Text(
-            "解析",
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-              fontFamily: 'Times New Roman',
-            ),
-          ),
-          Text(
-            answer ?? "暂无解析",
-            style: const TextStyle(
-              fontSize: 14,
-              fontFamily: 'Times New Roman',
-            ),
-          ),
-        ],
-      ),
-    )),
-  );
+        )),
+      ));
 }
 
 class _InnerState extends State<QuestionScreen> {
+  bool _onUndo(
+    int? previousIndex,
+    int currentIndex,
+    CardSwiperDirection direction,
+  ) {
+    debugPrint(
+      'The card $currentIndex was undod from the ${direction.name}',
+    );
+    return true;
+  }
+
+  final CardSwiperController controller = CardSwiperController();
   //这修改页面2的内容
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TDNavBar(title: '刷题界面', onBack: () {}),
+      floatingActionButton: Padding(
+          padding: const EdgeInsets.only(bottom: 150),
+          child: FloatingActionButton(
+              backgroundColor: const Color.fromARGB(255, 237, 237, 237),
+              hoverColor: const Color.fromARGB(255, 207, 207, 207),
+              child: const Icon(Icons.reply),
+              onPressed: () {
+                controller.undo();
+              })),
       body: Column(
         children: [
           Flexible(
@@ -199,6 +222,8 @@ class _InnerState extends State<QuestionScreen> {
                         q.question['w']));
                   }
                   return CardSwiper(
+                    controller: controller,
+                    onUndo: _onUndo,
                     cardsCount: cards.length,
                     cardBuilder: (context, index, percentThresholdX,
                             percentThresholdY) =>
@@ -276,15 +301,18 @@ class _InnerState extends State<QuestionScreen> {
                           slideTransitionFrom: SlideTransitionFrom.bottom,
                           builder: (context) {
                             return TDPopupBottomDisplayPanel(
-                              closeClick: () {
-                                Navigator.maybePop(context);
-                              },
-                              child: const Text("111"),
-                            );
+                                closeClick: () {
+                                  Navigator.maybePop(context);
+                                },
+                                child: const SingleChildScrollView(
+                                    child: Column(
+                                  children: [
+                                    Text("疑难题集"),
+                                  ],
+                                )));
                           }));
 
                       const EdgeInsets.only(bottom: 15, top: 15);
-                  
                     },
                     child: const Padding(
                       padding: EdgeInsets.only(bottom: 15, top: 15),
